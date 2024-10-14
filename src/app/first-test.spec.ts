94096ccd6efd7cb0f8f0c127425a678ab7dea45b
Michaël Cloots <michael.cloots@thomasmore.be>
2024-10-14 21:07:25 +0200

    comment first example test

--- src/app/first-test.spec.ts
+++ src/app/first-test.spec.ts
@@ -1,22 +1,22 @@
-describe('my first test', () => {
-    //body of the test
-    let sut; //system under test
+// describe('my first test', () => {
+//     //body of the test
+//     let sut; //system under test
 
-    //Code to reset state of test to clean state = initial state
-    //Executed before every test
-    beforeEach(() => {
-        sut = {};
-    });
+//     //Code to reset state of test to clean state = initial state
+//     //Executed before every test
+//     beforeEach(() => {
+//         sut = {};
+//     });
 
-    //First test (strings in describe and it are appended in test log!)
-    it('should be true if true', () => {
-        //arrange
-        sut.a = false;
+//     //First test (strings in describe and it are appended in test log!)
+//     it('should be true if true', () => {
+//         //arrange
+//         sut.a = false;
 
-        //act
-        sut.a = true;
+//         //act
+//         sut.a = true;
 
-        //assert
-        expect(sut.a).toBe(true);
-    })
-});
+//         //assert
+//         expect(sut.a).toBe(true);
+//     })
+// });
