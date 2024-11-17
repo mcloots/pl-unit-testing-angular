2c5af449b482e347b99c91ffabd17c61e72e0876
Michaël Cloots <michael.cloots@thomasmore.be>
2024-11-17 14:31:56 +0100

    control flow

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { MessageService } from '../message.service';
-import { NgIf, NgFor } from '@angular/common';
+
 
 @Component({
     selector: 'app-messages',
     templateUrl: './messages.component.html',
     styleUrls: ['./messages.component.css'],
     standalone: true,
-    imports: [NgIf, NgFor]
+    imports: []
 })
 export class MessagesComponent implements OnInit {
 
